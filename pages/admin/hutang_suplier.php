c14ef2fe4dd3d5538c7d6546e4c43abeb5c01c35
tauhid234 <syifatauhid@gmail.com>
2022-01-05 13:53:05 +0700

    PENAMAAN MODUL HUTANG SUPLIER

--- pages/admin/hutang_suplier.php
+++ pages/admin/hutang_suplier.php
@@ -41,12 +41,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Data Pembelian</h1>
+            <h1 class="m-0">Hutang Suplier</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Pembelian</a></li>
-              <li class="breadcrumb-item active">Data Pembelian</li>
+              <li class="breadcrumb-item"><a href="#">Hutang</a></li>
+              <li class="breadcrumb-item active">Hutang Suplier</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
